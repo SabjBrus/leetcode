bd801f564bcb3019db96a64573fe59a32fecd950
Борис Жуков <boris10264@gmail.com>
2023-12-16 17:48:31 +0300

    add remove duplicases 2

---
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left, right = 0, 0
+
+        while right < len(nums):
+            count = 1
+            while right + 1 < len(nums) and nums[right] == nums[right + 1]:
+                right += 1
+                count += 1
+
+            for i in range(min(2, count)):
+                nums[left] = nums[right]
+                left += 1
+            right += 1
+
+        return left
+
+
+result = Solution()
+
+print(result.removeDuplicates([1, 1, 1, 2, 2, 3]))
